8e1fb7b0cdd9bd8ee51f7bd4a5bc498f7b8ee07a
Wei-Cheng Hsieh <esambino.hsieh@gmail.com>
2013-12-20 11:40:10 +0800

    leap practice

---
+++ js/load.js
@@ -0,0 +1,33 @@
+﻿//document.write('<script src="/js/vendor/jquery.min.js"></script>');
+//document.write('<script src="/js/vendor/jquery-1.8.3.js"></script>');
+//document.write('<script src="/js/vendor/modernizr.js"></script>');
+
+//Particle Slider
+//document.write('<script src="/js/vendor/particleslider.js"></script>');
+
+//3D
+document.write('<script src="/js/vendor/three-60.min.js"></script>');
+//document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/three.js/r61/three.min.js"></script>');
+//document.write('<script src="/js/vendor/PointerLockControls.js"></script>');
+
+//Leap Motion
+document.write('<script src="/js/vendor/leap.min.js"></script>');
+//document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/leapjs/0.1.2/leap.min.js"></script>');
+
+//GSAP
+//document.write('<script src="/js/vendor/TweenMax.min.js"></script>');
+
+//Scroll 
+//document.write('<script src="/js/vendor/jquery.superscrollorama.js"></script>');
+//document.write('<script src="/js/vendor/jquery.parallax.min.js"></script>');
+
+//Easing
+//document.write('<script src="/js/vendor/jquery.easing.min.js"></script>');
+
+//Lazyload
+//document.write('<script src="/js/vendor/jquery.lazyload.min.js"></script>');
+
+//Prefix
+document.write('<script src="/js/vendor/prefixfree.min.js"></script>');
+document.write('<script src="/js/main.js" type="text/javascript"></script>');
+//document.write('<script src="/js/main_particle.js" type="text/javascript"></script>');
